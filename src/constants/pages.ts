6b29040de9fbf71a2174f5546d0e29822306a099
tamuraryoya <tamuraryoya@team-lab.com>
2020-04-30 23:19:34 +0900

    📝 Fixed url typo

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -1,7 +1,7 @@
 /** ページURL一覧 */
 export const PAGES = {
   /** おしらせ記事 */
-  NEWS_POST: '/news/post:id',
+  NEWS_POST: '/news/post/:id',
   /** おしらせカテゴリ */
   NEWS_CATEGORY: '/news/category/:id',
 };
