40215dbc9bd6dadd53ff5d38082e940c5ecedb51
tamuraryoya <tamuraryoya@team-lab.com>
2020-04-30 18:24:55 +0900

    🐛 Fixed typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,24 +7,25 @@ require('dotenv').config({ path: '.env' });
 module.exports = {
   siteMetadata: {
     title: '灯まる屋',
-    description: '',
+    description:
+      '千葉市稲毛区の居酒屋 灯まる屋。毎日新鮮な食材でおもてなしいたします。',
   },
   plugins: [
     'gatsby-plugin-typescript',
-    'gastby-plugin-sass',
+    'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-graphql-codegen',
       options: {
-        fileName: 'src/types/graphql.d.ts'
-      }
+        fileName: 'src/types/graphql.d.ts',
+      },
     },
     {
       resolve: 'gatsby-source-contentful',
-        options: {
-          spaceId: process.env.CONTENTFUL_SPACE_ID,
-          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        }
-    }
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 };
